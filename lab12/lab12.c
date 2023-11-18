b94c85d68e7c0fee3868c59a48d8da84b48469d4
vovan-ivanoff <nnyo-yo@yandex.ru>
2023-11-18 15:04:46 +0300

    Иванов В 12 лаба

---
+++ lab12/lab12.c
@@ -0,0 +1,26 @@
+//проверить есть ли смжные цифры в разрядах
+#include <stdio.h>
+
+int IsRep(int a){
+    int prevD = 10;
+    int isValid = 0;
+    a *= a > 0 ? 1 : -1; // хехе, модуль в 1 строчку
+    while (a > 0) {
+        int digit = a % 10;
+        if (prevD == digit) {
+            isValid = 1;
+            break;
+        }
+        a /= 10;
+        prevD = digit;
+    }
+    return isValid;
+}
+
+int main(){
+    int a = 0;
+    while (scanf("%d", &a) == 1){
+        printf("%d\n", IsRep(a));
+    }
+    return 0;
+}
